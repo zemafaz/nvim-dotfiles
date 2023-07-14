906481a66eaff0410731319a9d9f180767723356
José Azevedo <josemfazevedo.b@hotmail.com>
2023-07-14 14:39:05 +0100

    Added missing folder

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,66 @@
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+
+lsp.set_preferences({
+    sign_icons = {
+        error = "E",
+        warn = "W",
+        hint = "H",
+        info = "I"
+    }
+})
+
+lsp.ensure_installed({
+    "pyright",
+    "bashls",
+    "lua_ls"
+})
+
+local cmp = require('cmp')
+
+
+local cmp_mappings = lsp.defaults.cmp_mappings({})
+
+-- disable completion with tab
+cmp_mappings['<Tab>'] = nil
+cmp_mappings['<S-Tab>'] = nil
+
+-- disable Enter to confirm
+cmp_mappings['<CR>'] = nil
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings,
+    preselect = cmp.PreselectMode.None
+})
+
+lsp.on_attach(function(client, bufnr)
+
+    local bufopts = {buffer = bufnr, remap = false}
+
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+    vim.keymap.set('i', "<C-h>", function() vim.lsp.buf.signature_help() end, bufopts)
+
+    vim.keymap.set('n', '<space>ef', vim.diagnostic.open_float, bufopts)
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, bufopts)
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, bufopts)
+    vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, bufopts)
+end)
+
+lsp.nvim_workspace()
+
+lsp.setup()
---
+++ after/plugin/telescope.lua
@@ -0,0 +1,10 @@
+require("telescope").setup({
+    -- defaults = {
+    --     initial_mode = "normal" 
+    -- }
+})
+
+vim.keymap.set("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>")
+vim.keymap.set("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
+vim.keymap.set("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>")
+vim.keymap.set("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>")

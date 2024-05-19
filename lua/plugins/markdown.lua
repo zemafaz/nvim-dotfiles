62e26c96565990d4e55ef269a5667fd8c508f851
Jose Azevedo <josemfazevedo.b@hotmail.com>
2024-05-19 20:39:49 +0100

    Modified markdown-preview config

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -3,4 +3,11 @@ return {
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
+    init = function()
+        vim.g.mkdp_auto_start = 0
+        vim.g.mkdp_auto_close = 0
+    end,
+    keys = {
+        { "<leader>mp", "<cmd>MarkdownPreview<cr>", mode = "n" }
+    }
 }

519bb00fe5abf02c329ba9517245210d8dd39df6
Jose Azevedo <josemfazevedo.b@hotmail.com>
2024-05-19 17:19:52 +0100

    Markdown only wrap

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,11 +1,1 @@
 vim.o.wrap = true
-
-local function nnoremap(lhs, rhs, opts)
-	opts = vim.tbl_extend("force",
-			{noremap=true},
-            opts or {}
-	)
-	vim.keymap.set("n", lhs, rhs, opts)
-end
-
-nnoremap("<leader>p", "<cmd>PasteImg<cr>")

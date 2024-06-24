f273a63e451e36c1ec42de6560f9fe2885bdb709
Jose Azevedo <josemfazevedo.b@hotmail.com>
2024-06-24 15:32:09 +0100

    Added image clipboard support

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,13 +1,27 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-    init = function()
-        vim.g.mkdp_auto_start = 0
-        vim.g.mkdp_auto_close = 0
-    end,
-    keys = {
-        { "<leader>mp", "<cmd>MarkdownPreview<cr>", mode = "n" }
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+        init = function()
+            vim.g.mkdp_auto_start = 0
+            vim.g.mkdp_auto_close = 0
+        end,
+        keys = {
+            { "<leader>mp", "<cmd>MarkdownPreview<cr>", mode = "n" }
+        },
+    },
+    {
+        "ekickx/clipboard-image.nvim",
+        keys = {
+            { "<leader>pi", "<cmd>PasteImg<cr>", mode = "n" }
+        },
+        opts = {
+            default = {
+                img_dir = "./assets",
+                img_dir_txt = "assets",
+            }
+        },
     }
 }
